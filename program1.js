ae93dac186d9847d8e93c6899db5b23080d8d468
Sajid Syed <syedsajeed6969.com>
2023-02-01 21:54:38 +0530

    comment

---
+++ program1.js
@@ -0,0 +1,21 @@
+/* name, school, grade, section, rollno and the marks */
+
+let name = "Mahesh Gude";
+let school = "Delhi Public School";
+let grade = "A";
+let section = "B";
+let rollno = 101;
+let english = 86;
+let science = 90;
+let computer = 82;
+
+console.log("--------Sᴛᴜᴅᴇɴᴛ Report Card------");
+console.log(" Name          :",name);
+console.log(" School        :",school);
+console.log(" Grade         :",grade);
+console.log(" Section       :",section);
+console.log(" Roll no       :",rollno);
+console.log(" English  marks:",english);
+console.log(" Science marks :",science);
+console.log(" Computer marks:",computer);
+console.log("---------------------------------");
---
+++ program2.js
@@ -0,0 +1,12 @@
+let g="goku"; 
+let h="hii"; 
+let n=90.5;
+
+console.log(g+h); console.log(typeof(g+h));
+//string + string = string
+
+console.log(g+n); console.log(typeof(g+n));
+//string + no = string
+
+// no + no= no
+
